46b7a29ac4c995f39a23425c7d77b919f1f6dfe8
rustam.medvedev@bpium.ru <medvedrust@mail.ru>
2022-07-21 21:04:00 +0300

    fix(api): columns adding

--- src/store/api/index.ts
+++ src/store/api/index.ts
@@ -60,7 +60,7 @@ export const projectApi = createApi({
         method: 'post',
         body: column
       }),
-      invalidatesTags: ["Columns"]
+      invalidatesTags: ["Boards"]
     })
   }),
 })
