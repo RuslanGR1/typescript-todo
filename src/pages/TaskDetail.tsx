0604c305be238d1264e40cf5e4f59b805f94e49f
Ruslan G <almetfly@gmail.com>
2022-07-27 19:43:21 +0300

    add(TaskItem): allow to open task in modal

--- src/pages/TaskDetail.tsx
+++ src/pages/TaskDetail.tsx
@@ -1,20 +1,39 @@
-import { FC } from "react";
-import { useParams } from "react-router-dom";
+import React, { FC, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { useGetTaskQuery } from "store/api";
 
 interface TaskDetailProps {}
 
 const TaskDetail: FC<TaskDetailProps> = () => {
-  const { taskId } = useParams();
+  const { taskId, boardId } = useParams();
+  const [visible, setVisible] = useState<boolean>(true);
   const { data: task } = useGetTaskQuery(taskId);
   console.log("Task opened", taskId);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!visible) {
+      navigate(`/board/${boardId}`);
+    }
+  }, [visible]);
+
+  const onModalClick = (e: React.MouseEvent<HTMLElement>) => {
+    e.stopPropagation();
+  };
 
   return (
-    <div className="container mt-5 mx-auto">
-      <div className="bg-gray-200 p-5 rounded shadow w-[400px] h-[600px]">
+    <div
+      className="absolute top-0 right-0 flex justify-center items-center mx-auto w-screen h-screen bg-slate-100 overflow-hidden"
+      style={{ background: "rgba(0, 0, 0, 0.5)" }}
+      onClick={() => setVisible(false)}
+    >
+      <div
+        onClick={onModalClick}
+        className="bg-gray-200 p-5 rounded shadow w-[400px] h-[600px]"
+      >
         <div className="mb-4">Task#{taskId}</div>
         <textarea
-          className="border p-2 min-h-[150px] min-w-[300px]"
+          className="border p-2 min-h-[150px] min-w-[300px] outline-none bg-gray-100"
           value={task?.title}
         />
       </div>
